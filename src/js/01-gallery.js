aa9f0597d9923019ab8d3808aa4b6bf778d8ff1d
SebastianZasadni <emblelele@gmail.com>
2023-02-16 22:26:52 +0100

    feat task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const createGallery = galleryItems
+.map(item => `<li class="gallery__item"><a href="${item.original}" class="gallery__link"><img src="${item.preview}" 
+class="gallery__image" alt="${item.description}"></a></li>`)
+ .join("");  
+ gallery.insertAdjacentHTML("beforeend", createGallery);
+ const lightbox = new SimpleLightbox(".gallery a", {  captionsData: "alt",  captionDelay: 250,});
+
