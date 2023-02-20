e9fe7b69de473b3141769c4e2030b83bb1275b34
SebastianZasadni <emblelele@gmail.com>
2023-02-20 18:41:30 +0100

    feat task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+
+const player = new Player('vimeo-player', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('timeupdate', 
+_.throttle((function() {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem('videoplayer-current-time', seconds);
+         }).catch(function(error) {
+            console.log("Error");
+        });
+ }), 1000));
+ 
+const currentSeconds = localStorage.getItem('videoplayer-current-time');
+
+ player.setCurrentTime(currentSeconds);
 
